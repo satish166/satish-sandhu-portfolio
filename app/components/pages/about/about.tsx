90d20b4d6a56021e2cb40a7e9106556b77af53ea
Satish Kumar <harsh.kumar@souremash.com>
2025-12-30 19:03:10 +0530

    changes

---
+++ app/components/pages/about/about.tsx
@@ -0,0 +1,61 @@
+
+import "./about.scss";
+
+import Image from "next/image";
+
+import MyImage from "../../../assets/images/my-profile-img.jpg";
+
+export default function About() {
+  return (
+    <>
+      <section className="container about-section">
+           <div className="about-content">
+              <h2 className="heading">About</h2>
+              <h6 className="">I am a Frontend Developer with 5 years of experience in building scalable, high-performance, and user-centric web applications. I specialize in React, Angular, Next.js, and Salesforce Commerce Cloud (SFCC), with strong expertise in modern styling solutions like SCSS and Tailwind CSS. I focus on writing clean, maintainable code and creating seamless user experiences across devices.</h6>
+           </div>
+            <div className="personal-info">
+           <div className="row align-items-center">
+            <div className="col-md-4">
+               <div className="about-image">
+                <Image src={MyImage} alt="s" className="h-auto" />
+               </div>
+            </div>
+            <div className="col-md-8">
+              <h3>UI/UX Designer & Web Developer.</h3>
+              <div className="prsonal-details">
+                <ul>
+                  <li>
+                    <p className="font-semibold">Birthday:</p>
+                    <p>10 Jan 2000</p>
+                  </li>
+                  <li>
+                    <p className="font-semibold">Phone:</p>
+                    <p><a href="tel:8278860269">+91 8278860269</a></p>
+                  </li>
+                  <li>
+                    <p className="font-semibold">Age:</p>
+                    <p>25 Years</p>
+                  </li>
+                  <li>
+                    <p className="font-semibold">Email:</p>
+                    <p><a href="mailto:sandhusatish166@gmail.com">sandhusatish166@gmail.com</a></p>
+                  </li>
+                  <li>
+                    <p className="font-semibold">Degree:</p>
+                    <p>Bachelor of Computer Applications (BCA)</p>
+                  </li>
+                  <li>
+                    <p className="font-semibold">Address:</p>
+                    <p>Una (H.P)</p>
+                  </li>
+                  
+                </ul>
+              </div>
+            </div>
+           </div>
+            </div>
+           
+      </section>
+    </>
+  );
+}
---
+++ app/components/pages/contact/contact.tsx
@@ -0,0 +1,100 @@
+import "./contact.scss";
+
+import Image from "next/image";
+import Address from "../../../assets/images/address.svg";
+import Phone from "../../../assets/images/phone.svg";
+import Mail from "../../../assets/images/mail.svg";
+import Linkdin from "../../../assets/images/linkdin.svg";
+import Whatsapp from "../../../assets/images/whatsapp.svg";
+
+export default function Contact() {
+  return (
+    <>
+      <section className="contact-section">
+        <div className="work-content container">
+            <div className="row">
+              <div className="left-section col-md-5">
+                <h2 className="heading">Contact Us
+              </h2>
+              <div className="contact-us">
+                <ul>
+                  <li className="list-item">
+                     <Image  src={Address} alt="Hero" className="h-auto contact-us-icons" />
+                     <div className="">
+                        <h6>Address</h6>
+                        <p>Una (H.P)</p>
+                     </div>
+                  </li>
+                  <li className="list-item">
+                    <Image  src={Phone} alt="Hero" className="h-auto contact-us-icons" />
+                     <div className="">
+                        <h6>Call Us</h6>
+                        <p><a href="tel:8278860269">8278860269</a></p>
+
+                     </div>
+                  </li>
+                  <li className="list-item">
+                    <Image  src={Mail} alt="Hero" className="h-auto contact-us-icons" />
+                     <div className="">
+                        <h6>Address</h6>
+                        <p>Una (H.P)</p>
+                     </div>
+                  </li>
+                </ul>
+
+                  <ul className="social-links">
+                    <li>
+                      <a href="">
+                       <Image  src={Linkdin} alt="Hero" className="h-auto contact-us-icons" />
+                      </a>
+                    </li>
+                    <li> 
+                      <a href="https://wa.me/8278860269">
+                        <Image  src={Whatsapp} alt="Hero" className="h-auto contact-us-icons" />
+                      </a>
+                    </li>
+                  </ul>
+              </div>
+              </div>
+              <div className="right-section col-md-12">
+                <div className="row">
+                  <div className="col-md-5">
+    
+                  </div>
+                  <div className="col-md-7">
+                    <div className="get-in-touch">
+                          <h2 className="heading">Let's Work Together</h2>
+                          <p>Have a project in mind or want to collaborate? I'm just a message away</p>
+                          <form action="" method="get">
+                              <div className="field">
+                                <label htmlFor="Name">Name</label>
+                                <input type="text" placeholder="Enter Your Name" />
+                              </div>
+                              <div className="field">
+                                <label htmlFor="Name">Email</label>
+                                <input type="email" placeholder="Enter Your Name" />
+                              </div>
+                              <div className="field">
+                                <label htmlFor="Name">Message</label>
+                                <textarea name="Message" id="message" placeholder="Enter Message">
+                                  
+                                </textarea>
+                              </div>
+                              <div className="field">
+                              <button className="button button-primary" type="submit">Send Message</button>
+                              </div>
+
+                          </form>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+        </div>
+
+      </section>
+    </>
+  );
+}
